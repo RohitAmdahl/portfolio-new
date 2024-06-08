086e8dd91ee73da8919b5c8d27dcadd326a876ad
rohitAmdahl <rohit_designer@outlook.com>
2024-06-08 16:57:46 +0200

    navbar styles

--- app/components/header/Navbar.tsx
+++ app/components/header/Navbar.tsx
@@ -2,27 +2,28 @@ import Image from "next/image";
 import logo from "@/app/images/logo.png";
 import Link from "next/link";
 import { MdOutlineFileDownload } from "react-icons/md";
+import TopSection from "../home/TopSection";
 
 export default function Navbar() {
   return (
-    <header className="bg-secondary  ">
+    <header className="bg-secondary py-4 ">
       <div className="container max-w-5xl mx-auto">
-        <nav className="w-full flex justify-between items-center p-4">
+        <nav className="w-full flex justify-between items-center px-4  ">
           <div>
             <Image
-              className="max-w-[50px] w-full"
+              className="max-w-[30px] w-full"
               src={logo}
               alt="logo"
             ></Image>
           </div>
-          <div className="max-w-[150px] w-full">
+          <div className="max-w-[100px] w-full">
             <Link
               href="https://discord.com/828741507258646636"
               target="_blank"
               title="Discord"
-              className="bg-blue-500  hover:bg-blue-700 text-white font-bold py-3 px-4 flex justify-center items-center gap-2 rounded-xl shadow-shadow hover:shadow-inner duration-75 ease-in-out shadow-black text-lg"
+              className="bg-blue-500 ring-4 ring-blue-500  hover:bg-blue-700 text-white font-bold py-2 px-2 flex justify-center items-center gap-2 rounded-xl shadow-shadow hover:shadow-inner duration-75 ease-in-out shadow-black text-md "
             >
-              <MdOutlineFileDownload size={24} />
+              <MdOutlineFileDownload size={20} />
               CV
             </Link>
           </div>
