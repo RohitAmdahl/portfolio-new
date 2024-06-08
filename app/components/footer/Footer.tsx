7bdf240039d211ae83c2c5ba82a113a504a41271
rohitAmdahl <rohit_designer@outlook.com>
2024-06-08 22:21:13 +0200

    --footer -- navbar fixed positon

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,3 +1,21 @@
+import Link from "next/link";
+
 export default function Footer() {
-  return <footer className=" text-text">Footer</footer>;
+  return (
+    <footer className="bg-primary text-textColor mt-8">
+      <div className=" container mx-auto text-center max-w-4xl py-8   ">
+        <Link
+          href="https://www.linkedin.com/feed/"
+          className="hover:underline m-8"
+        >
+          Designed By
+          <span className="font-bold  text-textColor">
+            &copy; Rohit kumar Amdahl,
+          </span>
+        </Link>
+
+        <span>{new Date().getFullYear()} </span>
+      </div>
+    </footer>
+  );
 }
--- app/components/header/Navbar.tsx
+++ app/components/header/Navbar.tsx
@@ -6,28 +6,30 @@ import TopSection from "../home/TopSection";
 
 export default function Navbar() {
   return (
-    <header className="bg-secondary py-4 ">
-      <div className="container max-w-5xl mx-auto">
-        <nav className="w-full flex justify-between items-center px-4  ">
-          <div>
-            <Image
-              className="max-w-[30px] w-full"
-              src={logo}
-              alt="logo"
-            ></Image>
-          </div>
-          <div className="max-w-[100px] w-full">
-            <Link
-              href="https://discord.com/828741507258646636"
-              target="_blank"
-              title="Discord"
-              className="bg-blue-500 ring-4 ring-blue-500  hover:bg-blue-700 text-white font-bold py-2 px-2 flex justify-center items-center gap-2 rounded-xl shadow-shadow hover:shadow-inner duration-75 ease-in-out shadow-black text-md "
-            >
-              <MdOutlineFileDownload size={20} />
-              CV
-            </Link>
-          </div>
-        </nav>
+    <header className=" w-full mx-auto bg-secondary fixed p-4 top-0 ">
+      <div className=" max-w-5xl container mx-auto backdrop-blur-sm bg-secondary/30 ">
+        <div className="  ">
+          <nav className="w-full flex justify-between items-center px-4 ">
+            <div>
+              <Image
+                className="max-w-[35px] w-full"
+                src={logo}
+                alt=" rohit kumar amdahl logo"
+              ></Image>
+            </div>
+            <div className="max-w-[100px] w-full">
+              <Link
+                href="https://discord.com/828741507258646636"
+                target="_blank"
+                title="Discord"
+                className="bg-blue-500 ring-4 ring-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1 flex justify-center items-center gap-2 rounded-xl shadow-shadow hover:shadow-inner duration-75 ease-in-out shadow-black text-md "
+              >
+                <MdOutlineFileDownload size={20} />
+                CV
+              </Link>
+            </div>
+          </nav>
+        </div>
       </div>
     </header>
   );
