1cb642894a4821c94fd630658d3011a0cb766471
rohitAmdahl <rohit_designer@outlook.com>
2024-06-08 22:21:33 +0200

    component added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import TopSection from "./components/home/TopSection";
 import InfoSection from "./components/home/InfoSection";
+import TabButtons from "./components/tab/TabButtons";
 
 export default function Home() {
   return (
     <div>
       <TopSection />
       <InfoSection />
+      <TabButtons />
     </div>
   );
 }
