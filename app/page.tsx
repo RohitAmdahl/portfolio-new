9a87f9019a47bf4e41484137e3a3c06b6c4e03c4
rohitAmdahl <rohit_designer@outlook.com>
2024-06-08 16:57:28 +0200

    componets added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
+import TopSection from "./components/home/TopSection";
+import InfoSection from "./components/home/InfoSection";
 
 export default function Home() {
   return (
-    <section>
-      <div className=" bg- max-w-4xl container mx-auto h-16">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis quasi
-        saepe iste vel sapiente eaque deleniti. Laboriosam libero sed earum
-        maxime! Ipsa, rem hic! Est maiores totam odio optio deleniti!
-      </div>
-    </section>
+    <div>
+      <TopSection />
+      <InfoSection />
+    </div>
   );
 }
