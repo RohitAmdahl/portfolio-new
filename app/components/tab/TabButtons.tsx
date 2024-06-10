c2d8b94b6abcb61a5bd935a1f5be3e125331b9bf
rohitAmdahl <rohit_designer@outlook.com>
2024-06-11 01:05:27 +0200

    added components

--- app/components/tab/TabButtons.tsx
+++ app/components/tab/TabButtons.tsx
@@ -1,12 +1,18 @@
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+import AboutTab from "../about/AboutTab";
+import ArticlesTab from "../articles/ArticlesTab";
+import Projects from "../projects/Projects";
 const TabButtons = () => {
   return (
-    <TabGroup defaultIndex={0} className={"max-w-4xl container mx-auto "}>
+    <TabGroup
+      defaultIndex={0}
+      className={"max-w-4xl container mx-auto shadow-sm "}
+    >
       <TabList className="grid  grid-cols-3 items-center py-3 px-2 text-textColor text-xl rounded-full ring-4 ring-primary gap-4 bg-secondary ">
         <Tab className={"border-r-2  border-bg-secondary rounded-2xl"}>
           About
         </Tab>
-        <Tab className={"border-r-2 border-bg-secondary rounded-2xl"}>
+        <Tab className={"border-r-2 border-bg-secondary rounded-2xl "}>
           Article
         </Tab>
         <Tab className={"border-r-2 border-bg-secondary rounded-2xl"}>
@@ -14,15 +20,19 @@ const TabButtons = () => {
         </Tab>
       </TabList>
       <TabPanels>
-        <div className="bg-green-200 text-black">
-          <TabPanel>Content 1</TabPanel>
-        </div>
-        <div className="bg-green-200 text-black">
-          <TabPanel>Content 2</TabPanel>
-        </div>
-        <div className="bg-green-200 text-black">
-          <TabPanel>Content 3</TabPanel>
-        </div>
+        <TabPanel className={"p-4 text-textColor text-lg "}>
+          <AboutTab />
+        </TabPanel>
+
+        <TabPanel className={"p-4 text-textColor text-lg "}>
+          <div className="grid grid-col-3 ">
+            <ArticlesTab />
+          </div>
+        </TabPanel>
+
+        <TabPanel className={"p-4 text-textColor text-lg "}>
+          <Projects />
+        </TabPanel>
       </TabPanels>
     </TabGroup>
   );
